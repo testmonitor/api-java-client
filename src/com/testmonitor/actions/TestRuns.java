6a4f64b618e71e4b9bfbee9aee6cba7335fca92e
stefanius <sgrootveld@testmonitor.com>
2021-11-04 15:52:20 +0100

    Add mergeTestCases

--- src/com/testmonitor/actions/TestRuns.java
+++ src/com/testmonitor/actions/TestRuns.java
@@ -223,7 +223,7 @@ public class TestRuns
     }
 
     /**
-     * Add users to a test run
+     * Add test cases to a test run
      *
      * @param testRun The test run you want to update
      *
@@ -232,6 +232,30 @@ public class TestRuns
     public TestRun addTestCases(TestRun testRun, List<Integer> testCaseIds)
     {
         List<NameValuePair> params = new ArrayList<>();
+
+        for (Integer testCaseId : testCaseIds) {
+            params.add(new BasicNameValuePair("test_cases[]", testCaseId.toString()));
+        }
+
+        JSONObject response = this.connector.put("test-runs/" + testRun.getId(), params);
+
+        HashMap<String, Object> updatedTestRun = (HashMap<String, Object>) response.getJSONObject("data").toMap();
+
+        return TestRunParser.parse(updatedTestRun);
+    }
+
+    /**
+     * Merge new test cases to a test run
+     *
+     * @param testRun The test run you want to update
+     *
+     * @return A new instance of the test run
+     */
+    public TestRun mergeTestCases(TestRun testRun, List<Integer> testCaseIds)
+    {
+        List<NameValuePair> params = new ArrayList<>();
+
+        params.add(new BasicNameValuePair("merge", "1"));
 
         for (Integer testCaseId : testCaseIds) {
             params.add(new BasicNameValuePair("test_cases[]", testCaseId.toString()));
